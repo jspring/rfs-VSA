ef22d0a1298e961a38e3b76a1f478c33b73fcfe4
John Spring <jspring@berkeley.edu>
2017-09-05 20:23:28 -0700

    modified:   scripts/safepace_set_speed.php Modified to an executable shell script POSTing every 10 seconds

--- scripts/safepace_set_speed.php
+++ scripts/safepace_set_speed.php
@@ -1,10 +1,7 @@
-<!DOCTYPE html> 
-<head>
-<title>SafePace 650 Set Speed test</title>
-<meta http-equiv="refresh" content="10" />
-</head>
-<body>
+#!/usr/bin/php
+
 <?php
+    while(1) {
 	$context = stream_context_create(array(
 	'http' => array(
 	'method' => "POST", 
@@ -12,40 +9,20 @@
 	'header' => "x-api-key: f2be074061f00df705d9fb2a540a4f1f" . "\r\n"
 	)
 	));
-	echo "context: $context";
-?>
-	<p></p>
-<?php
+//	echo "context: $context";
 	$speed = file_get_contents('/var/www/html/php/speed.txt');
 	$request_url = 'https://api.streetsoncloud.com/sv1/set-speed-limit/location/3068/speed-limit/'.$speed;
-?>
-<p></p>
-<?php
-	echo "request_url: $request_url";
-?>
-<p></p>
-<?php
+//	echo "request_url: $request_url";
 	//Try using file_get_contents to get location information
 	$xml_string = file_get_contents($request_url, false, $context);
-	var_dump($http_response_header);
-?>
-<p></p>
-<?php
-	echo "Try var_dump of xml_string: ";  
+//	var_dump($http_response_header);
+//	echo "Try var_dump of xml_string: ";  
 	var_dump($xml_string);
-?>
-<p></p>
-<?php
-	echo "Try var_dump of context: ";  
-	var_dump($context);
-?>
-<p></p>
-<?php
-	echo "Try var_dump of : request_url";  
+//	echo "Try var_dump of context: ";  
+//	var_dump($context);
+//	echo "Try var_dump of : request_url";  
 	var_dump($request_url);
+//	phpinfo();
+	sleep(10);
+    }
 ?>
-<p></p>
-<?php
-	phpinfo();
-?>
-</body>
