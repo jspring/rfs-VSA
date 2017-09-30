1d7422e26613b12970898fcf133d9ca84c592c3b
John Spring <jspring@berkeley.edu>
2017-09-29 18:46:09 -0700

    modified:   opt_vsa.c Standardized initialization of arrays to {0} Began change from db_urms_status* variables to lds{*][*] variables

--- src/opt_vsa.c
+++ src/opt_vsa.c
@@ -76,6 +76,10 @@ int main(int argc, char *argv[])
 	db_urms_status_t controller_data[NUM_LDS];  //See warning at top of file
 	db_urms_status2_t controller_data2[NUM_LDS];  //See warning at top of file
 	db_urms_status3_t controller_data3[NUM_LDS];  //See warning at top of file
+	loop_data_t lds[NUM_LDS][NUM_LOOPNAMES] = {0}; 	// Row (NUM_LDS) = controller index, 
+							// column (NUM_LOOPNAMES) = loop index
+							// Each lds element has speed, occupancy, 
+							// flow, and error status (see loop_data_t definition)
 	db_vsa_ctl_t db_vsa_ctl;
 
 	int option;
@@ -88,21 +92,21 @@ int main(int argc, char *argv[])
 	char *domain = DEFAULT_SERVICE; // usually no need to change this
 	int xport = COMM_OS_XPORT;      // set correct for OS in sys_os.h
 //	int verbose = 0;
-	agg_data_t mainline_out[NUM_CYCLE_BUFFS][SecSize] =  {{{0}},{{0}}};      // data aggregated section by section
-	agg_data_t onramp_out[NUM_CYCLE_BUFFS][NumOnRamp] = {{{0}},{{0}}};      // data aggregated section by section
-    agg_data_t onramp_queue_out[NUM_CYCLE_BUFFS][NumOnRamp] = {{{0}},{{0}}};      // data aggregated section by section
-	agg_data_t offramp_out[NUM_CYCLE_BUFFS][NUM_OFFRAMPS] = {{{0}},{{0}}};  // data aggregated section by section
+	agg_data_t mainline_out[NUM_CYCLE_BUFFS][SecSize] =  {0};      // data aggregated section by section
+	agg_data_t onramp_out[NUM_CYCLE_BUFFS][NumOnRamp] = {0};      // data aggregated section by section
+    agg_data_t onramp_queue_out[NUM_CYCLE_BUFFS][NumOnRamp] = {0};      // data aggregated section by section
+	agg_data_t offramp_out[NUM_CYCLE_BUFFS][NUM_OFFRAMPS] = {0};  // data aggregated section by section
     
-    agg_data_t mainline_out_f[SecSize] = {{0}};        // save filtered data to this array
-	agg_data_t onramp_out_f[NumOnRamp] = {{0}};        // save filtered data to this array
-	agg_data_t offramp_out_f[NUM_OFFRAMPS] = {{0}};    // save filtered data to this array
-	agg_data_t onramp_queue_out_f[NumOnRamp] = {{0}};  // save filtered data queue detector data to this array
+    agg_data_t mainline_out_f[SecSize] = {0};        // save filtered data to this array
+	agg_data_t onramp_out_f[NumOnRamp] = {0};        // save filtered data to this array
+	agg_data_t offramp_out_f[NUM_OFFRAMPS] = {0};    // save filtered data to this array
+	agg_data_t onramp_queue_out_f[NumOnRamp] = {0};  // save filtered data queue detector data to this array
 	 
 	
-	agg_data_t controller_mainline_data[NUM_LDS] = {{0}};     // data aggregated controller by controller 
-	agg_data_t controller_onramp_data[NUM_ONRAMPS] = {{0}};                 // data aggregated controller by controller
-	agg_data_t controller_onramp_queue_detector_data[NUM_ONRAMPS] = {{0}};
-	agg_data_t controller_offramp_data[NUM_OFFRAMPS] = {{0}};               // data aggregated controller by controller
+	agg_data_t controller_mainline_data[NUM_LDS] = {0};     // data aggregated controller by controller 
+	agg_data_t controller_onramp_data[NUM_ONRAMPS] = {0};                 // data aggregated controller by controller
+	agg_data_t controller_onramp_queue_detector_data[NUM_ONRAMPS] = {0};
+	agg_data_t controller_offramp_data[NUM_OFFRAMPS] = {0};               // data aggregated controller by controller
 	float hm_speed_prev [NUM_LDS] = {1.0};               // this is the register of harmonic mean speed in previous time step
 	float mean_speed_prev [NUM_LDS] = {1.0};             // this is the register of mean speed in previous time step
     float density_prev [NUM_LDS] = {0};             // this is the register of density in previous time step
@@ -151,7 +155,7 @@ int main(int argc, char *argv[])
 				break;
 		}
 	}
-	memset(controller_data, 0, NUM_LDS * (sizeof(db_urms_status_t)));//See warning at top of file
+	memset(lds, 0, NUM_LDS * NUM_LOOPNAMES * (sizeof(loop_data_t)));
 	memset(controller_data2, 0, NUM_LDS * (sizeof(db_urms_status2_t)));//See warning at top of file
 	memset(controller_data3, 0, NUM_LDS * (sizeof(db_urms_status3_t)));//See warning at top of file
 
@@ -176,7 +180,7 @@ int main(int argc, char *argv[])
 
 	if (init_sw==1)
 	{
-		Init();  
+//		Init();  
 		Init_sim_data_io();
 		init_sw=0;
 	}
@@ -184,9 +188,10 @@ int main(int argc, char *argv[])
 		num_controller_vars = 2; //Use just the first two controllers in the list, i.e. 10.29.248.108 and 10.254.25.113.
 
 	for (i = 0; i < num_controller_vars; i++){   //See warning at top of file
-		db_clt_read(pclt, db_vds_list[i].id, db_vds_list[i].size, &controller_data[i]);
-		db_clt_read(pclt, db_vds_list[i+14].id, db_vds_list[i+14].size, &controller_data2[i]);
-		db_clt_read(pclt, db_vds_list[i+28].id, db_vds_list[i+28].size, &controller_data3[i]);
+		db_clt_read(pclt, db_vds_list[i].id, db_vds_list[i].size, &lds[i][0]);
+//		db_clt_read(pclt, db_vds_list[i].id, db_vds_list[i].size, &controller_data[i]);
+//		db_clt_read(pclt, db_vds_list[i+14].id, db_vds_list[i+14].size, &controller_data2[i]);
+//		db_clt_read(pclt, db_vds_list[i+28].id, db_vds_list[i+28].size, &controller_data3[i]);
 	}
 
 //BEGIN MAIN FOR LOOP HERE
@@ -196,9 +201,10 @@ int main(int argc, char *argv[])
 	cycle_index++;
 	cycle_index = cycle_index % NUM_CYCLE_BUFFS;
 	for (i = 0; i < num_controller_vars; i++){  //See warning at top of file
-		db_clt_read(pclt, db_vds_list[i].id, db_vds_list[i].size, &controller_data[i]);
-		db_clt_read(pclt, db_vds_list[i+14].id, db_vds_list[i+14].size, &controller_data2[i]);
-		db_clt_read(pclt, db_vds_list[i+28].id, db_vds_list[i+28].size, &controller_data3[i]);
+		db_clt_read(pclt, db_vds_list[i].id, db_vds_list[i].size, &lds[i][0]);
+//		db_clt_read(pclt, db_vds_list[i].id, db_vds_list[i].size, &controller_data[i]);
+//		db_clt_read(pclt, db_vds_list[i+14].id, db_vds_list[i+14].size, &controller_data2[i]);
+//		db_clt_read(pclt, db_vds_list[i+28].id, db_vds_list[i+28].size, &controller_data3[i]);
 	}
 
 /*#################################################################################################################
@@ -211,30 +217,31 @@ int main(int argc, char *argv[])
 	print_timestamp(dbg_st_file_out, pts); // #1 print out current time step to file
  
 	for(i=0;i<NUM_LDS;i++){
-		printf("opt_crm: IP %s onramp1 passage volume %d demand vol %d offramp volume %d\n", controller_strings[i][2], controller_data[i].metered_lane_stat[0].passage_vol, controller_data[i].metered_lane_stat[0].demand_vol, controller_data3[i].additional_det[0].volume);
+//		printf("opt_crm: IP %s onramp1 passage volume %d demand vol %d offramp volume %d\n", controller_strings[i][2], lds[i][P1_e].rawvolume, controller_data[i].metered_lane_stat[0].demand_vol, controller_data3[i].additional_det[0].volume);
+		printf("opt_crm: IP %s onramp1 passage volume %d\n", controller_strings[i][2], lds[i][P1_e].rawvolume);
 		
 		// min max function bound the data range and exclude nans.
-        controller_mainline_data[i].agg_vol = Mind(12000.0, Maxd( 1.0, flow_aggregation_mainline(&controller_data[i], &confidence[i][0]) ) );
-		controller_mainline_data[i].agg_occ = Mind(90.0, Maxd( 1.0, occupancy_aggregation_mainline(&controller_data[i], &confidence[i][0]) ) );
+//        controller_mainline_data[i].agg_vol = mind(12000.0, maxd( 1.0, flow_aggregation_mainline(&controller_data[i], &confidence[i][0]) ) );
+		controller_mainline_data[i].agg_occ = mind(90.0, maxd( 1.0, occupancy_aggregation_mainline(&controller_data[i], &confidence[i][0]) ) );
 		 
 		float_temp = hm_speed_aggregation_mainline(&controller_data[i], hm_speed_prev[i], &confidence[i][0]);
 		if(float_temp < 0){
 			printf("Error %f in calculating harmonic speed for controller %s\n", float_temp, controller_strings[i][2]);
 			float_temp = hm_speed_prev[i];
 		}
-		controller_mainline_data[i].agg_speed = Mind(150.0, Maxd( 1.0, float_temp) );
+		controller_mainline_data[i].agg_speed = mind(150.0, maxd( 1.0, float_temp) );
 		 
 		float_temp = mean_speed_aggregation_mainline(&controller_data[i], mean_speed_prev[i], &confidence[i][0]);
 		if(float_temp < 0){
 			printf("Error %f in calculating mean speed for controller %s\n", float_temp, controller_strings[i][2]);
 			float_temp = mean_speed_prev[i];
 		}
-		controller_mainline_data[i].agg_mean_speed = Mind(150.0, Maxd( 1.0, float_temp) );
+		controller_mainline_data[i].agg_mean_speed = mind(150.0, maxd( 1.0, float_temp) );
 
 		if(confidence[i][0].num_total_vals > 0)
 			printf("Confidence for controller %s mainline %f total_vals %f good vals %f\n", controller_strings[i][2], (float)confidence[i][0].num_good_vals/confidence[i][0].num_total_vals, (float)confidence[i][0].num_total_vals, (float)confidence[i][0].num_good_vals);
         
-        controller_mainline_data[i].agg_density = Mind(125.0,Maxd( 1.0,  density_aggregation_mainline(controller_mainline_data[i].agg_vol, controller_mainline_data[i].agg_speed, density_prev[i]) ) );
+        controller_mainline_data[i].agg_density = mind(125.0,maxd( 1.0,  density_aggregation_mainline(controller_mainline_data[i].agg_vol, controller_mainline_data[i].agg_speed, density_prev[i]) ) );
 		
 		hm_speed_prev[i] = controller_mainline_data[i].agg_speed;
         mean_speed_prev[i] = controller_mainline_data[i].agg_mean_speed;
@@ -249,8 +256,8 @@ int main(int argc, char *argv[])
         //fprintf(dbg_st_file_out,"\n");
         
 		// assign off-ramp data to array
-		controller_offramp_data[i].agg_vol =  Mind(6000.0, Maxd( 0, flow_aggregation_offramp(&controller_data3[i], &confidence[i][2]) ) );
-        controller_offramp_data[i].agg_occ =  Mind(100.0, Maxd( 0, occupancy_aggregation_offramp(&controller_data3[i], &confidence[i][2]) ) );            
+		controller_offramp_data[i].agg_vol =  mind(6000.0, maxd( 0, flow_aggregation_offramp(&controller_data3[i], &confidence[i][2]) ) );
+        controller_offramp_data[i].agg_occ =  mind(100.0, maxd( 0, occupancy_aggregation_offramp(&controller_data3[i], &confidence[i][2]) ) );            
 		controller_offramp_data[i].turning_ratio = turning_ratio_offramp(controller_offramp_data[i].agg_vol,controller_mainline_data[i-1].agg_vol);
 		if(confidence[i][2].num_total_vals > 0)
 			printf("Confidence for controller %s offramp %f total_vals %f good vals %f\n", controller_strings[i][2], (float)confidence[i][2].num_good_vals/confidence[i][2].num_total_vals, (float)confidence[i][2].num_total_vals, (float)confidence[i][2].num_good_vals);
@@ -263,13 +270,13 @@ int main(int argc, char *argv[])
         //fprintf(dbg_st_file_out,"\n");
         
 		// assign on-ramp data to array
-		controller_onramp_data[i].agg_vol = Mind(6000.0, Maxd( 0, flow_aggregation_onramp(&controller_data[i], &confidence[i][1]) ) );
+		controller_onramp_data[i].agg_vol = mind(6000.0, maxd( 0, flow_aggregation_onramp(&controller_data[i], &confidence[i][1]) ) );
 		if(confidence[i][1].num_total_vals > 0)
 			printf("Confidence for controller %s onramp flow %f total_vals %f good vals %f\n", controller_strings[i][2], (float)confidence[i][1].num_good_vals/confidence[i][1].num_total_vals, (float)confidence[i][1].num_total_vals, (float)confidence[i][1].num_good_vals);
-		controller_onramp_data[i].agg_occ = Mind(100.0, Maxd( 0, occupancy_aggregation_onramp(&controller_data[i], &controller_data2[i], &confidence[i][1]) ) );
+		controller_onramp_data[i].agg_occ = mind(100.0, maxd( 0, occupancy_aggregation_onramp(&controller_data[i], &controller_data2[i], &confidence[i][1]) ) );
 		// data from on-ramp queue detector
-		controller_onramp_queue_detector_data[i].agg_vol = Mind(6000.0, Maxd( 0, flow_aggregation_onramp_queue(&controller_data[i], &controller_data2[i], &confidence[i][1]) ));
-        controller_onramp_queue_detector_data[i].agg_occ = Mind(100.0, Maxd( 0, occupancy_aggregation_onramp_queue(&controller_data[i], &controller_data2[i], &confidence[i][1]) ));
+		controller_onramp_queue_detector_data[i].agg_vol = mind(6000.0, maxd( 0, flow_aggregation_onramp_queue(&controller_data[i], &controller_data2[i], &confidence[i][1]) ));
+        controller_onramp_queue_detector_data[i].agg_occ = mind(100.0, maxd( 0, occupancy_aggregation_onramp_queue(&controller_data[i], &controller_data2[i], &confidence[i][1]) ));
 
 		if(confidence[i][1].num_total_vals > 0)
 			printf("Confidence for controller %s onramp occupancy (queue) %f total_vals %f good vals %f\n", controller_strings[i][2], (float)confidence[i][1].num_good_vals/confidence[i][1].num_total_vals, (float)confidence[i][1].num_total_vals, (float)confidence[i][1].num_good_vals);
@@ -287,8 +294,8 @@ int main(int argc, char *argv[])
 
     // VSA control code start from here
 	 	
-		det_data_4_contr(time);	
-		get_meas(time);
+//		det_data_4_contr(time);	
+//		get_meas(time);
 		for(i=0; i<NUM_SIGNS; i++){
 			db_vsa_ctl.vsa[i]= 0; //NOTE to Chengju: Assign variable speeds here
 		    db_clt_write(pclt, DB_ALL_SIGNS_VAR, sizeof(db_vsa_ctl_t), &db_vsa_ctl);
@@ -297,6 +304,7 @@ int main(int argc, char *argv[])
 	
 	Finish_sim_data_io();
 	      	
+	}
 	return 0;
 }
 
